4239e46d41308dc5d7c177a8b99aa34582be2240
FlabyKare <103658067+FlabyKare@users.noreply.github.com>
2022-08-20 16:19:06 +0300

    header_finished

--- js/script.js
+++ js/script.js
@@ -1,13 +1,19 @@
 const menu_btn = document.querySelector('.mobile_menu')
 const menu_wrapper = document.querySelector('.mobile_menu_wrapper')
 const menu_close = document.querySelector('.closeMenu')
-const body_hidden = document.querySelector('body')
+const body_hidden = document.querySelectorAll(".mobile_opened");
 
 menu_btn.addEventListener('click', () => {
     menu_wrapper.classList.toggle('right_0')
-    body_hidden.classList.toggle('body_hide')
+    appenedClasses()
 })
 menu_close.addEventListener('click', () => {
     menu_wrapper.classList.toggle('right_0')
-    body_hidden.classList.toggle('body_hide')
+    appenedClasses()
 })
+
+const appenedClasses = ()=>{
+    body_hidden.forEach((item) => {
+        item.classList.toggle('body_hide')
+      });
+}
