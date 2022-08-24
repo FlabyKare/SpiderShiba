2afd93acca704e6785d05bc22a569d0796232af4
FlabyKare <103658067+FlabyKare@users.noreply.github.com>
2022-08-24 15:48:25 +0300

    Уменьшил скорость прокрутки бегущего текста

--- js/script.js
+++ js/script.js
@@ -1,13 +1,19 @@
 const menu_btn = document.querySelector('.mobile_menu')
+const menu_link = document.querySelector(".menu_link");
 const menu_wrapper = document.querySelector('.mobile_menu_wrapper')
 const menu_close = document.querySelector('.closeMenu')
 const body_hidden = document.querySelectorAll(".mobile_opened");
+
 
 menu_btn.addEventListener('click', () => {
     menu_wrapper.classList.toggle('right_0')
     appenedClasses()
 })
 menu_close.addEventListener('click', () => {
+    menu_wrapper.classList.toggle('right_0')
+    appenedClasses()
+})
+menu_link.addEventListener('click', () => {
     menu_wrapper.classList.toggle('right_0')
     appenedClasses()
 })
