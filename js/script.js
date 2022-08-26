201397c552a6a0901cbac6cbc11b58a47051b6f4
FlabyKare <103658067+FlabyKare@users.noreply.github.com>
2022-08-26 18:11:38 +0300

    accordionFinished

--- js/script.js
+++ js/script.js
@@ -8,19 +8,102 @@ const menuWrapper = () => {
    menu_wrapper.classList.toggle("right_0");
 };
 const appenedClasses = () => {
-    body_hidden.forEach((item) => {
-       item.classList.toggle("body_hide");
-    });
- };
+   body_hidden.forEach((item) => {
+      item.classList.toggle("body_hide");
+   });
+};
 
 function toggleMenu() {
-    menuWrapper();
-    appenedClasses();
+   menuWrapper();
+   appenedClasses();
 }
 
 menu_btn.addEventListener("click", toggleMenu);
 menu_close.addEventListener("click", toggleMenu);
-menuLinks.forEach( link => link.addEventListener("click", toggleMenu))
+menuLinks.forEach((link) => link.addEventListener("click", toggleMenu));
+
+
+
+
+// const accordionItem = document.querySelector('.accordion__item')
+// const accordionItem2 = document.querySelector('.accordion__item2')
+// const accordionItem3 = document.querySelector('.accordion__item3')
+// const accordionItem4 = document.querySelector('.accordion__item4')
+
+
+
+// function toggleHeight1(){
+//     accordionItem.classList.toggle("heightFitContent");
+// }
+// function toggleHeight2(){
+//     accordionItem2.classList.toggle("heightFitContent");
+// }
+// function toggleHeight3(){
+//     accordionItem3.classList.toggle("heightFitContent");
+// }
+// function toggleHeight4(){
+//     accordionItem4.classList.toggle("heightFitContent");
+// }
+
+// accordionItem.addEventListener("click", ()=> {
+//     toggleHeight1()
+// })
+// accordionItem2.addEventListener("click", ()=> {
+//     toggleHeight2()
+// })
+// accordionItem3.addEventListener("click", ()=> {
+//     toggleHeight3()
+// })
+// accordionItem4.addEventListener("click", ()=> {
+//     toggleHeight4()
+// })
+
+
+
+$(document).ready(function(){
+    $('.accordion-list > li > .answer').hide();
+      
+    $('.accordion-list > li').click(function() {
+      if ($(this).hasClass("active")) {
+        $(this).removeClass("active").find(".answer").slideUp();
+      } else {
+        $(".accordion-list > li.active .answer").slideUp();
+        $(".accordion-list > li.active").removeClass("active");
+        $(this).addClass("active").find(".answer").slideDown();
+      }
+      return false;
+    });
+    
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 window.onload = function () {
    document.body.classList.add("loaded_hiding");
@@ -30,87 +113,80 @@ window.onload = function () {
    }, 500);
 };
 
-
 function onEntry(entry) {
-    entry.forEach(change => {
+   entry.forEach((change) => {
       if (change.isIntersecting) {
-       change.target.classList.add('element-show');
+         change.target.classList.add("element-show");
       }
-    });
-  }
-  
-  let options = {
-    threshold: [0.5] };
-  let observer = new IntersectionObserver(onEntry, options);
-  let elements = document.querySelectorAll('.element-animation');
-  
-  for (let elm of elements) {
-    observer.observe(elm);
-  }
-
-
-  const anchors = document.querySelectorAll('a[href*="#"]')
-
-for (let anchor of anchors) {
-  anchor.addEventListener('click', function (e) {
-    e.preventDefault()
-    
-    const blockID = anchor.getAttribute('href').substr(1)
-    
-    document.getElementById(blockID).scrollIntoView({
-      behavior: 'smooth',
-      block: 'start'
-    })
-  })
+   });
 }
 
+let options = {
+   threshold: [0.5],
+};
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll(".element-animation");
+for (let elm of elements) {
+   observer.observe(elm);
+}
+const anchors = document.querySelectorAll('a[href*="#"]');
+for (let anchor of anchors) {
+   anchor.addEventListener("click", function (e) {
+      e.preventDefault();
 
-$('.slider_wrapper').slick({
-    infinite: true,
-    slidesToShow: 4,
-    slidesToScroll: 2,
-    arrows: false,
-    dots: true,
-    responsive: [
-        {
-          breakpoint: 1500,
-          settings: {
+      const blockID = anchor.getAttribute("href").substr(1);
+
+      document.getElementById(blockID).scrollIntoView({
+         behavior: "smooth",
+         block: "start",
+      });
+   });
+}
+
+$(".slider_wrapper").slick({
+   infinite: true,
+   slidesToShow: 4,
+   slidesToScroll: 2,
+   arrows: false,
+   dots: true,
+   responsive: [
+      {
+         breakpoint: 1500,
+         settings: {
             infinite: true,
             slidesToShow: 3,
             slidesToScroll: 2,
             arrows: false,
-            dots: true
-          }
-        },
-        {
-            breakpoint: 1100,
-            settings: {
-              infinite: true,
-              slidesToShow: 2,
-              slidesToScroll: 2,
-              arrows: false,
-              dots: true
-            }
-          },
-          {
-            breakpoint: 768,
-            settings: {
-              infinite: true,
-              slidesToShow: 1,
-              slidesToScroll: 1,
-              arrows: false,
-              dots: true
-            }
-          },
+            dots: true,
+         },
+      },
+      {
+         breakpoint: 1100,
+         settings: {
+            infinite: true,
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            arrows: false,
+            dots: true,
+         },
+      },
+      {
+         breakpoint: 768,
+         settings: {
+            infinite: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+            dots: true,
+         },
+      },
+   ],
+});
 
-      ]
-  });
-
-
-  $('.mobile_tokenomiks_items').slick({
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: false,
-    dots: false,
-  });
+$(".mobile_tokenomiks_items").slick({
+   infinite: true,
+   slidesToShow: 1,
+   slidesToScroll: 1,
+   arrows: false,
+   dots: false,
+});
